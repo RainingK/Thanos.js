886ba1c0b80a978bbd5801fc875ca502329d3d0f
Ammar <ammar_98@live.com>
2019-11-29 23:28:35 +0400

    Server will now add the files to local storage and then delete them once the task is done.

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
-const dragDrop = require('drag-drop')
 const multer = require('multer');
+const fs = require('fs');
 
 const app = express();
-const upload = multer();
+const upload_directory = __dirname + '\\uploads';
 
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage })
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
@@ -16,8 +27,20 @@ app.get('/', function(req, res) {
 
 // Maximum 20 Files Can Be Uploaded At A Time
 app.post('/', upload.array('files', 20) ,function(req, res, next) {
-    console.log(req.files);
-    
+    file_array = req.files
+    console.log(file_array);
+
+    // Deletes the files in the directory once everything is done.
+    fs.readdir(upload_directory, (err, files) => {
+        if (err) throw err;
+        console.log(upload_directory);
+        for (const file of files) {
+          fs.unlink(path.join(upload_directory, file), err => {
+            if (err) throw err;
+          });
+        }
+    });
+
     res.redirect('/')
 })
 
