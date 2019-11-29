529db444965e8cb6eb5301deeb264e8d0c0fc378
Ammar <ammar_98@live.com>
2019-11-29 22:36:39 +0400

    Added 'multer' package to view uploaded files. Created a form to upload multiple files.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const dragDrop = require('drag-drop')
+const multer = require('multer');
+
+const app = express();
+const upload = multer();
 
 var port = process.env.PORT || 3000;
 
@@ -9,6 +13,13 @@ app.use(express.static(__dirname));
 app.get('/', function(req, res) {
     res.sendFile( __dirname + '/views/index.html')
 });
+
+// Maximum 20 Files Can Be Uploaded At A Time
+app.post('/', upload.array('files', 20) ,function(req, res, next) {
+    console.log(req.files);
+    
+    res.redirect('/')
+})
 
 app.listen(port, function(){
     console.log('Your node js server is running on localhost:' + port);
